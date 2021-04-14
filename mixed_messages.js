40e1e787d9e29bcb7f13246eeb9e1b48b70e503a
Nick <nirikawadev@gmail.com>
2021-04-14 01:23:11 -0700

    Display the random information.

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,6 +1,10 @@
 
 const firstNames = ["Brogan", "Jamen", "Locus", "Nico", "Repper", "Denagelo", "Mogis"];
 const lastNames = ["Hillhand", "Applepatch", "Cliprigger", "Fangwhistle", "Burnwillow"];
-const appearance = ["Portly", "Slender", "Emaciated", "Tired", "Flimsy", "Strong", "Rotund"];
+const appearance = ["portly", "slender", "emaciated", "tired", "flimsy", "strong", "rotund"];
 
-console.log("Bye!");
+randFirst = Math.floor(Math.random() * firstNames.length);
+randLast = Math.floor(Math.random() * lastNames.length);
+randAppearance = Math.floor(Math.random() * appearance.length);
+
+console.log(`Their name is ${firstNames[randFirst]} ${lastNames[randLast]} and they are ${appearance[randAppearance]}.`);
