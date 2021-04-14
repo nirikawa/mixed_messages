26583b5bd8e72638249223479f308995aa4202fc
Nick <nirikawadev@gmail.com>
2021-04-13 23:54:25 -0700

    Add the arrays of names and appearances

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,4 +1,6 @@
 
-const firstNames = ["Greg", "Jamen", "Locus", "Nico", "Repper", "Denagelo", "Mogis"];
+const firstNames = ["Brogan", "Jamen", "Locus", "Nico", "Repper", "Denagelo", "Mogis"];
+const lastNames = ["Hillhand", "Applepatch", "Cliprigger", "Fangwhistle", "Burnwillow"];
+const appearance = ["Portly", "Slender", "Emaciated", "Tired", "Flimsy", "Strong", "Rotund"];
 
 console.log("Bye!");
